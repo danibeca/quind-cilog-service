3b32c193390d7ef7e2613b8f07484c8fda288874
Daniel BC <danibeca@okazo.co>
2017-11-05 23:45:24 -0500

    Adding get jobs by component.

--- app/Utils/Transformers/ExistingJobTransformer.php
+++ app/Utils/Transformers/ExistingJobTransformer.php
@@ -9,7 +9,7 @@ class ExistingJobTransformer extends Transformer
     public function transform($component)
     {
         return [
-            'id' =>$component['existing_jobs']['phase_job_id']
+            'id' =>$component['existing_jobs'][0]['phase_job_id']
         ];
     }
 }
